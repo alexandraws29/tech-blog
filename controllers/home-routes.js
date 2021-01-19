04ff44d7a824ac9edc1ac3d587a8011703a5dd6f
Alexa <alexandraws29@outlook.com>
2021-01-18 20:38:41 -0700

    fixed error in home routes, adjusted settings in server.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,30 +39,37 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return
+    }
+    res.render('login');
+});
+router.get('/post/:id', (req, res) => {
     Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'post_text',
-            'title',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at,'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
+    where: {
+        id: req.params.id
+    },
+    attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at'
+    ],
+    include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at,'],
+            include: {
                 model: User,
                 attributes: ['username']
             }
-        ]
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
     })
     .then(dbPostData => {
         if(!dbPostData) {
--- server.js
+++ server.js
@@ -2,23 +2,22 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('.utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 const session = require('express-session');
-const app = express();
-const PORT = process.env.PORT || 3001
 const SequelizeStore = require('connect-session-sequelize')(session.store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
+const app = express();
+const PORT = process.env.PORT || 3001
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -31,5 +30,5 @@ app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
