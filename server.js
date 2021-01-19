d05e81bb8f766ff31314252e5a6bc3858300ba9a
Alexa <alexandraws29@outlook.com>
2021-01-18 23:05:42 -0700

    attempting to debug sequelize

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
-const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3001
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.store);
 
 const sess = {
@@ -31,8 +16,15 @@ const sess = {
     db: sequelize
   })
 };
+const app = express();
+const PORT = process.env.PORT || 3001
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
 app.use(routes);
